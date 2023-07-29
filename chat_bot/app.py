4ac27535b565cb890f199e4975e0034c41248856
shkr715 <58281491+shkr715@users.noreply.github.com>
2023-07-29 16:28:41 +0900

    Update app.py

--- chat_bot/app.py
+++ chat_bot/app.py
@@ -8,7 +8,7 @@ openai.api_key = st.secrets.OPENAIAPI.openai_api_key
 # st.session_stateを使いメッセージのやりとりを保存
 if "messages" not in st.session_state:
     st.session_state["messages"] = [
-        {"role": "system", "content": "あなたはどんな入力にも親父ギャグで返すAIです。面白おかしく返答してください。"}
+        {"role": "system", "content": "あなたは関西弁でどんなことにも返答する優秀なAIです。"}
         ]
 
 # チャットボットとやりとりする関数
@@ -35,7 +35,7 @@ if "messages" not in st.session_state:
     st.session_state.condition = ""
 
 #タイトル
-st.title("Dad Joke ChatBot")
+st.title("関西人ChatBot")
 
 st.text_area("お喋りしましょか〜", key="user_input")
 # 送信ボタンがクリックするとOpenAIに問い合わせる
